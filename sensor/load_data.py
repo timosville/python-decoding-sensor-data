90289c3aba1c4c5b9b0f79f8258fc4f0b15a1251
Timothy Koech <timosville@gmail.com>
2020-08-15 14:33:40 +0300

    Read data files

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -6,3 +6,7 @@ import os
 def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+
+    for sensor_file in sensor_files:
+        with open(sensor_file) as data_file:
+            data_file = csv.DictReader(data_file, delimiter=',')
