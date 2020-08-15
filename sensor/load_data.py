ca4fb057225be239721230a4eb0c6ed697d7f3b4
Timothy Koech <timosville@gmail.com>
2020-08-15 14:37:31 +0300

    Fix error

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -9,4 +9,4 @@ def load_sensor_data():
 
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
-            data_file = csv.DictReader(data_file, delimiter=',')
+            data_reader = csv.DictReader(data_file, delimiter=',')
